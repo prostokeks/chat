18ec48779d2f3a301b63748fae198ccf6fee151c
prostokeks <tov.camz@gmail.com>
2022-05-31 12:43:46 +0300

    Messages +

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,5 @@
 class Room < ApplicationRecord
   belongs_to :user
+  has_many :messages, -> {sorted},dependent: :destroy
   before_create { self.title = SecureRandom.hex(3) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_31_092339) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_31_093403) do
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
+    t.string "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title", null: false
@@ -31,5 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_31_092339) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "rooms", "users"
 end
